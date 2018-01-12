0b68dfebcb2146dd33e266bde67d7993639da72a
Joe Thorley <joe@poissonconsulting.ca>
2018-01-12 09:21:22 -0800

    test distributions

--- tests/testthat/test-burr.R
+++ tests/testthat/test-burr.R
@@ -12,23 +12,23 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-context("lgumbel")
+context("burr")
 
-test_that("rlgumbel", {
+test_that("rburr", {
   set.seed(99)
-  r <- rlgumbel(100000, location = 100, scale = 3)
+  r <- rburr(100000,1,1)
   expect_identical(length(r), 100000L)
-  expect_equal(mean(log(r)), 3 * 0.57721 + 100, tolerance = 0.001)
+  expect_equal(mean(r), 10.9, tolerance = 0.1)
 })
 
-test_that("pqlgumbel", {
-  expect_equal(log(qlgumbel(0.5, 3, 1)), 3.366513, tolerance = 0.000001)
-  expect_equal(plgumbel(exp(3), 3, 1), 0.3678794, tolerance = 0.0000001)
-  expect_equal(plgumbel(exp(4), 3, 1), 0.6922006, tolerance = 0.0000001)
-  expect_identical(plgumbel(qlgumbel(0.5, 3, 1), 3, 1), 0.5)
+test_that("pqburr", {
+  expect_equal(qburr(0.5, 1,1), 1, tolerance = 0.000001)
+  expect_equal(pburr(exp(3), 1,1), 0.9525741, tolerance = 0.0000001)
+  expect_equal(pburr(exp(4), 1, 1), 0.9820138, tolerance = 0.0000001)
+  expect_equal(pburr(qburr(0.5, 3, 1), 3, 1), 0.5)
 })
 
-test_that("dlgumbel", {
- expect_equal(dlgumbel(exp(3), 3, 1), 0.01831564, tolerance = 0.0000001)
- expect_equal(dlgumbel(exp(4), 3, 1), 0.004664011, tolerance = 0.0000001)
+test_that("dburr", {
+ expect_equal(dburr(0.5, 3, 1), 0.5925926, tolerance = 0.0000001)
+ expect_equal(dburr(0.6, 3, 1), 0.4577637, tolerance = 0.0000001)
 })
--- tests/testthat/test-llog.R
+++ tests/testthat/test-llog.R
@@ -12,23 +12,23 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-context("lgumbel")
+context("llog")
 
-test_that("rlgumbel", {
+test_that("rllog", {
   set.seed(99)
-  r <- rlgumbel(100000, location = 100, scale = 3)
+  r <- rllog(100000)
   expect_identical(length(r), 100000L)
-  expect_equal(mean(log(r)), 3 * 0.57721 + 100, tolerance = 0.001)
+  expect_equal(mean(log(r)), 1, tolerance = 0.1)
 })
 
-test_that("pqlgumbel", {
-  expect_equal(log(qlgumbel(0.5, 3, 1)), 3.366513, tolerance = 0.000001)
-  expect_equal(plgumbel(exp(3), 3, 1), 0.3678794, tolerance = 0.0000001)
-  expect_equal(plgumbel(exp(4), 3, 1), 0.6922006, tolerance = 0.0000001)
-  expect_identical(plgumbel(qlgumbel(0.5, 3, 1), 3, 1), 0.5)
+test_that("pqllog", {
+  expect_equal(log(qllog(0.5)), 1, tolerance = 0.000001)
+  expect_equal(pllog(exp(3)), 0.8807971, tolerance = 0.0000001)
+  expect_equal(pllog(exp(4)), 0.9525741, tolerance = 0.0000001)
+  expect_identical(pllog(qllog(0.5, 3, 1), 3, 1), 0.5)
 })
 
-test_that("dlgumbel", {
- expect_equal(dlgumbel(exp(3), 3, 1), 0.01831564, tolerance = 0.0000001)
- expect_equal(dlgumbel(exp(4), 3, 1), 0.004664011, tolerance = 0.0000001)
+test_that("dllog", {
+ expect_equal(dllog(exp(3), 3, 1), 0.003720046, tolerance = 0.0000001)
+ expect_equal(dllog(exp(4), 3, 1), 0.001200358, tolerance = 0.0000001)
 })
