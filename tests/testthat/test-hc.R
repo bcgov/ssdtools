70694c77bb0de0df1447a53551a6647c9fae20c7
joethorley <joe@poissonconsulting.ca>
2019-11-28 09:33:16 -0800

    test hc

--- tests/testthat/test-hc.R
+++ tests/testthat/test-hc.R
@@ -14,12 +14,34 @@
 
 context("hc")
 
-test_that("ssd_hc", {
-  boron_data <- ssdtools::boron_data
-  boron_lnorm <- ssd_fit_dist(boron_data[1:6, ])
+test_that("ssd_hc fitdist", {
   x <- ssd_hc(boron_lnorm, nboot = 10L)
   expect_is(x, "tbl")
   expect_identical(colnames(x), c("percent", "est", "se", "lcl", "ucl"))
   expect_identical(x$percent, 5L)
-  expect_equal(x$est, 1.502548, tolerance = 0.0000001)
+  expect_equal(x$est, 1.68117483775796)
+})
+
+test_that("ssd_hc fitdistcens", {
+  x <- ssd_hc(fluazinam_lnorm, nboot = 10L)
+  expect_is(x, "tbl")
+  expect_identical(colnames(x), c("percent", "est", "se", "lcl", "ucl"))
+  expect_identical(x$percent, 5L)
+  expect_equal(x$est, 1.74352219048516)
+})
+
+test_that("ssd_hc fitdists", {
+  x <- ssd_hc(boron_dists, nboot = 10L)
+  expect_is(x, "tbl")
+  expect_identical(colnames(x), c("percent", "est", "se", "lcl", "ucl"))
+  expect_identical(x$percent, 5L)
+  expect_equal(x$est, 1.25078185265013)
+})
+
+test_that("ssd_hc fitdistscens", {
+  x <- ssd_hc(fluazinam_dists, nboot = 10L)
+  expect_is(x, "tbl")
+  expect_identical(colnames(x), c("percent", "est", "se", "lcl", "ucl"))
+  expect_identical(x$percent, 5L)
+  expect_equal(x$est, 1.61325245103811)
 })
