bb9bf943ca21600ebc5c03a251ffba3429bd350e
joethorley <joe@poissonconsulting.ca>
2019-12-03 14:34:50 -0800

    added release reminders for licensing (#60)

---
+++ R/release.R
@@ -0,0 +1,6 @@
+release_questions <- function() {
+  c(
+    "Have you confirmed Apache 2.0 license at the top of all code files?",
+    "Have you confirmed Creative Commons license for all non-code files?"
+  )
+}
