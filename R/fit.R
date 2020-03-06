bc14866effc9067f32fd45dd444c6dbdc993afd4
joethorley <joe@poissonconsulting.ca>
2020-03-06 12:02:47 -0800

    remove call. = FALSE in wrn call.

--- R/fit.R
+++ R/fit.R
@@ -55,7 +55,7 @@ remove_errors <- function(dist_fit, name, computable, silent) {
   }
   sd <- dist_fit$result$sd
   if (is.null(sd) || any(is.na(sd))) {
-    if (!silent) wrn("Distribution ", name, " failed to compute standard errors (try rescaling the data or increasing the sample size).", call. = FALSE)
+    if (!silent) wrn("Distribution ", name, " failed to compute standard errors (try rescaling the data or increasing the sample size).")
     if (computable) {
       return(NULL)
     }
