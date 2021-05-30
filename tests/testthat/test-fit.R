f043dd1c052ecdf7fc1d5ae86d93a3524bb2b431
Joe Thorley <joe@poissonconsulting.ca>
2021-05-30 11:46:11 -0700

    add test for boron data

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -183,6 +183,23 @@ test_that("ssd_fit_dists warns of optimizer convergence code error", {
   )
 })
 
+test_that("ssd_fit_dists estimates for boron_data", {
+  data <- ssdtools::boron_data
+  dists <- ssd_dists()
+  dists <- dists[!dists %in% c("burrIII3", "gompertz")]
+  fits <- ssd_fit_dists(ssdtools::boron_data, dists = dists)
+  expect_identical(names(fits), dists)
+  
+  expect_equal(estimates(fits), 
+    list(gamma = list(scale = 25.1268319779061, shape = 0.950179460431249),
+         lgumbel = list(locationlog = 1.92263082409711, scalelog = 1.23223883525026),
+         llogis = list(locationlog = 2.62627625930417, scalelog = 0.740426376456358),
+         lnorm = list(meanlog = 2.56164496371788, sdlog = 1.24154032419128),
+         mx_llogis_llogis = list(locationlog1 = 0.896785336771335,
+                                 locationlog2 = 3.14917770255953, pmix = 0.255223497712533,
+                                 scalelog1 = 0.317650881482978, scalelog2 = 0.496708998267848),
+         weibull = list(scale = 23.5139783002509, shape = 0.966099901938021)))
+})
 
 test_that("fit_dist tiny llogis", {
   data <- ssdtools::boron_data
