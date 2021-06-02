3d463e43de6e91038c264f09b397d83d43af429a
Joe Thorley <joe@poissonconsulting.ca>
2021-06-02 10:07:48 -0700

    rearrange and document ggproto

--- R/ggproto.R
+++ R/ggproto.R
@@ -12,62 +12,55 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-#' Base ggproto Classes for ggplot2
+#' ggproto Classes for Plotting Species Sensitivity Data and Distributions
 #'
-#' @seealso [ggplot2::ggplot2-ggproto()]
+#' @seealso [ggplot2::ggproto()] and [ssd_plot_cdf()]
 #' @name ssdtools-ggproto
 NULL
 
 #' @rdname ssdtools-ggproto
-#' @format NULL
-#' @usage NULL
 #' @export
 StatSsdpoint <- ggproto(
   "StatSsdpoint", Stat,
+  required_aes = "x",
+  default_aes = aes(y = ..density..),
   compute_panel = function(data, scales) {
     data$density <- ssd_ecd(data$x)
     data
-  },
-  default_aes = aes(y = ..density..),
-  required_aes = "x"
+  }
 )
 
 #' @rdname ssdtools-ggproto
-#' @format NULL
-#' @usage NULL
 #' @export
 StatSsdsegment <- ggproto(
   "StatSsdsegment", Stat,
+  required_aes = c("x", "xend"),
+  default_aes = aes(y = ..density.., yend = ..density..),
   compute_panel = function(data, scales) {
     data$density <- ssd_ecd(rowMeans(data[c("x", "xend")], na.rm = TRUE))
     data
-  },
-  default_aes = aes(y = ..density.., yend = ..density..),
-  required_aes = c("x", "xend")
+  }
 )
 
 #' @rdname ssdtools-ggproto
-#' @format NULL
-#' @usage NULL
 #' @export
 GeomSsdpoint <- ggproto(
   "GeomSsdpoint", GeomPoint
 )
 
 #' @rdname ssdtools-ggproto
-#' @format NULL
-#' @usage NULL
 #' @export
 GeomSsdsegment <- ggproto(
   "GeomSsdsegment", GeomSegment
 )
 
 #' @rdname ssdtools-ggproto
-#' @format NULL
-#' @usage NULL
 #' @export
 GeomHcintersect <- ggproto(
   "GeomHcintersect", Geom,
+  required_aes = c("xintercept", "yintercept"),
+  default_aes = aes(colour = "black", size = 0.5, linetype = "dotted", alpha = NA),
+  draw_key = draw_key_path,
   draw_panel = function(data, panel_params, coord) {
     data$group <- 1:nrow(data)
     data$x <- data$xintercept
@@ -79,33 +72,20 @@ GeomHcintersect <- ggproto(
     
     data <- rbind(start, data, end)
     GeomPath$draw_panel(data, panel_params, coord)
-  },
-  
-  default_aes = aes(colour = "black", size = 0.5, linetype = "dotted", alpha = NA),
-  required_aes = c("xintercept", "yintercept"),
-  
-  draw_key = draw_key_path
+  }
 )
 
 #' @rdname ssdtools-ggproto
-#' @format NULL
-#' @usage NULL
 #' @export
 GeomXribbon <- ggproto(
   "GeomXribbon", Geom,
-  default_aes = aes(
-    colour = NA, fill = "grey20", size = 0.5, linetype = 1,
-    alpha = NA
-  ),
-  
   required_aes = c("y", "xmin", "xmax"),
-  
+  default_aes = aes(
+    colour = NA, fill = "grey20", size = 0.5, linetype = 1,alpha = NA),
   draw_key = draw_key_polygon,
-  
   handle_na = function(data, params) {
     data
   },
-  
   draw_group = function(data, panel_params, coord, na.rm = FALSE) {
     if (na.rm) data <- data[complete.cases(data[c("y", "xmin", "xmax")]), ]
     data <- data[order(data$group, data$y), ]
