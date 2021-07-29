f1c174eade7f1967e3c3b294378001326b533468
Joe Thorley <joe@poissonconsulting.ca>
2021-07-29 06:57:17 -0700

    invpareto test

--- tests/testthat/test-npars.R
+++ tests/testthat/test-npars.R
@@ -15,6 +15,6 @@
 test_that("npars", {
   expect_identical(npars(boron_lnorm), c(lnorm = 2L))
   expect_identical(npars(boron_dists), c(gamma = 2L, llogis = 2L, lnorm = 2L))
-  dists <- ssd_fit_dists(ssdtools::boron_data, dists = c("llogis_llogis", "lnorm_lnorm", "lnorm"))
-  expect_identical(npars(dists), c(llogis_llogis = 5L, lnorm_lnorm = 5L, lnorm = 2L))
+  dists <- ssd_fit_dists(ssdtools::boron_data, dists = c("llogis_llogis", "lnorm_lnorm", "lnorm", "invpareto"))
+  expect_identical(npars(dists), c(llogis_llogis = 5L, lnorm_lnorm = 5L, lnorm = 2L, invpareto = 1L))
 })
