b4c4d8a31da41c01a8743e48ad3d98852ceaf73e
Joe Thorley <joe@poissonconsulting.ca>
2021-08-11 14:42:28 -0700

    gompertz initial values inherently unstable

--- tests/testthat/test-gompertz.R
+++ tests/testthat/test-gompertz.R
@@ -24,17 +24,21 @@ test_that("bootstrap gompertz with problem data", {
   data <- data.frame(Conc = c(0.12, 1.8, 0.26, 0.5, 0.35, 1.7, 4.3, 3.2))
   fit <- ssdtools::ssd_fit_dists(data, dists = 'gompertz')
   set.seed(99)
-  hc <- ssd_hc(fit, ci = TRUE, nboot = 100, min_pboot = 0.8)
+  hc <- ssd_hc(fit, ci = TRUE, nboot = 11, min_pboot = 0.9)
   expect_snapshot_data(hc, "hc_prob")
 })
 
-test_that("fit gompertz with problem bootstrap data", {
-  data <- data.frame(
-    Conc = 
-      c(1.57878861060696, 1.1540195927281, 1.85714027655063, 0.293912308964471, 
-        3.08208825276762, 0.711069057349639, 1.0570773946438, 2.59236028395207
-      ))
-  fit <- ssdtools::ssd_fit_dists(data, dists = 'gompertz')
-  tidy <- tidy(fit)
-  expect_snapshot_data(tidy, "tidy_prob_boot")
+test_that("sgompertz completely unstable!", {
+  x <- c(3.15284072848962, 1.77947821504531, 0.507778085984185, 1.650387414067, 
+         1.00725113964435, 7.04244885481452, 1.32336941144339, 1.51533791792454
+  )
+  data <- data.frame(left = x, right = x, weight = 1)
+  set.seed(94)
+  expect_equal(ssdtools:::sgompertz(data),
+  list(log_location = -0.809751972284548, log_shape = -301.126320260922))
+  set.seed(99)
+  expect_equal(ssdtools:::sgompertz(data),
+               list(log_location = -0.96528645818605, log_shape = -2.6047441710778))
+  set.seed(100)
+  expect_error(ssdtools:::sgompertz(data))
 })
