5cb0f21d9861ab48b987fc263c13600d913a6063
Joe Thorley <joe@poissonconsulting.ca>
2023-05-15 11:46:51 -0700

    - Test table values to 8 significant digits.

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -43,7 +43,7 @@ expect_snapshot_plot <- function(x, name) {
 }
 
 expect_snapshot_data <- function(x, name) {
-  fun <- function(x) signif(x, digits = 10)
+  fun <- function(x) signif(x, digits = 8)
   x <- dplyr::mutate(x, dplyr::across(where(is.numeric), fun))
   path <- save_csv(x)
   testthat::expect_snapshot_file(path, paste0(name, ".csv"))
