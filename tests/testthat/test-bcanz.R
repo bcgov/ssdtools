b661686e66b933f300ae61446543efbba1e9f307
Joe Thorley <joe@poissonconsulting.ca>
2023-08-26 08:52:53 -0700

    test to autopilot

--- tests/testthat/test-bcanz.R
+++
@@ -1,9 +0,0 @@
-test_that("ssd_dists_bcanz works", {
-  expect_identical(ssd_dists_bcanz(), 
-                   c("gamma", "lgumbel", "llogis", 
-                     "lnorm", "lnorm_lnorm", "weibull"))
-})
-
-test_that("ssd_dists_bcanz works", {
-  expect_identical(ssd_dists_bcanz(), ssd_dists(bcanz = TRUE))
-})
