4818c3cf48fe58b07d714454c7d6325ac277669a
Joe Thorley <joe@poissonconsulting.ca>
2023-10-11 06:22:47 -0700

    restrict scope style_pkg to line breaks

--- scripts/build.R
+++ scripts/build.R
@@ -13,7 +13,10 @@
 #    limitations under the License.
 
 roxygen2md::roxygen2md()
-styler::style_pkg(filetype = c("R", "Rmd"))
+styler::style_pkg(
+  scope = "line_breaks",
+  filetype = c("R", "Rmd")
+)
 lintr::lint_package()
 
 devtools::test()
