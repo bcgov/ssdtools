fcfa578876b0ba154d9e2e91708c556b22ee2e9c
Joe Thorley <joe@poissonconsulting.ca>
2023-11-08 12:37:31 -0800

    switch to default tolerance where 1e-10

--- tests/testthat/test-hc-root.R
+++ tests/testthat/test-hc-root.R
@@ -22,7 +22,7 @@ test_that("hc root lnorm", {
   hc_root <- ssd_hc(fits, average = TRUE, root = TRUE)
   expect_identical(hc_average$est, hc_dist$est)
   expect_identical(hc_average$est, hc_dist$est)
-  expect_equal(hc_root, hc_average, tolerance = 1e-10)
+  expect_equal(hc_root, hc_average)
   expect_equal(hc_root$est, 1.68117483988121, tolerance = 1e-6)
   
   testthat::expect_snapshot({
@@ -37,8 +37,8 @@ test_that("hc root all", {
   hc_average <- ssd_hc(fits, average = TRUE)
   hc_root <- ssd_hc(fits, average = TRUE, root = TRUE)
   expect_equal(hc_root, hc_average, tolerance = 1e-1)
-  expect_equal(hc_average$est, 1.24151700389853, tolerance = 1e-10)
-  expect_equal(hc_root$est, 1.25678623624403, tolerance = 1e-10)
+  expect_equal(hc_average$est, 1.24151700389853)
+  expect_equal(hc_root$est, 1.25678623624403)
   testthat::expect_snapshot({
     hc_root
   })
