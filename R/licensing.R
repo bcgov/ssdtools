049b0b2ff32b38ee1cb2482ae5f0ebd3cd707b9a
Joe Thorley <joe@poissonconsulting.ca>
2023-11-08 14:57:05 -0800

    added licensing_md() function and moved below references.
    closes #81 and closes #301

---
+++ R/licensing.R
@@ -0,0 +1,33 @@
+#' Licensing Markdown
+#' 
+#' A string of markdown code indicating the licensing of the code 
+#' and documentation
+#' @export
+#' @examples
+#' licensing_md()
+licensing_md <- function() {
+"## Licensing
+
+  The code is released under 
+  the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+  
+  Copyright 2023 Province of British Columbia,
+   Environment and Climate Change Canada and 
+   Australian Government Department of 
+   Climate Change, Energy, the Environment and Water
+  
+  Licensed under the Apache License, Version 2.0 (the \"License\");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at 
+  
+  https://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an \"AS IS\" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  
+  The documentation is released under the
+  [CC BY 4.0 License](https://creativecommons.org/licenses/by/4.0/)"
+}
