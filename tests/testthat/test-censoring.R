af7b34ebdd1c8a162847b9a807bc945be8fdb8b6
Joe Thorley <joe@poissonconsulting.ca>
2025-05-15 09:43:55 +0800

    add censoring test

--- tests/testthat/test-censoring.R
+++ tests/testthat/test-censoring.R
@@ -15,7 +15,7 @@ test_that("left, right and interval censoring works ", {
   
   expect_warning(hcaverage <- ssd_hc(fit, ci = FALSE, average = TRUE, parametric = TRUE), "^Model averaged estimates cannot be calculated for censored data when the distributions have different numbers of parameters\\.$")
   expect_snapshot_data(hcaverage, "hcaverage")
-
+  
   set.seed(42)
   # FIXME - should return tibble with 1 row even if NAs
   expect_error(expect_warning(hcaveragenonparametric <- ssd_hc(fit, ci = TRUE, average = TRUE, parametric = FALSE, nboot = 10, min_pboot = 0.5), "^Model averaged estimates cannot be calculated for censored data when the distributions have different numbers of parameters\\.$"))
@@ -42,3 +42,19 @@ test_that("left, right and interval censoring works same number of parameter ", 
   hcaveragenonparametric <- ssd_hc(fit, ci = TRUE, average = TRUE, parametric = FALSE, nboot = 10)
   expect_snapshot_data(hcaveragenonparametric, "hcaveragenonparametric2")
 })
+
+test_that("stability of bccanz dists with heavy censoring", {
+  
+  set.seed(10)
+  
+  data <- data.frame(Conc = rlnorm(30, 1))
+  data$right <- data$Conc + rlnorm(30, sdlog = 2)
+  data$right[runif(15, 1, nrow(data))] <- Inf  
+
+  fit <- ssd_fit_dists(data = data, right = "right")
+  
+  hcheavy <- ssd_hc(fit, average = FALSE)
+  expect_snapshot_data(hcheavy, "hcheavy")
+})
+
+    
