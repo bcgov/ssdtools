fc000bc22c1aa3ffe6b0b74394129d34a2297bef
Joe Thorley <joe@poissonconsulting.ca>
2025-06-06 15:29:49 +0800

    refactoring hcp functions

---
+++ R/hcp-ind.R
@@ -0,0 +1,32 @@
+hcp_ind_weight <- function(hcp, weight) {
+  hcp <- mapply(
+    function(x, y) {
+      x$wt <- y
+      x
+    },
+    x = hcp, y = weight,
+    USE.NAMES = FALSE, SIMPLIFY = FALSE
+  )
+  hcp <- bind_rows(hcp)
+  hcp[c("dist", "value", "est", "se", "lcl", "ucl", "wt", "nboot", "pboot", "samples")]
+}
+
+hcp_ind <- function(x, value, ci, level, nboot, min_pboot, estimates,
+                         data, rescale,
+                         weighted, censoring, min_pmix, range_shape1,
+                         range_shape2, parametric,
+                         control, est_method, ci_method, hc, save_to, samples, fun) {
+  
+  hcp <- purrr::map(x, .ssd_hcp_tmbfit, weight = 1,
+                    value = value, ci = ci, level = level, nboot = nboot,
+                    min_pboot = min_pboot,
+                    data = data, rescale = rescale, weighted = weighted, censoring = censoring,
+                    min_pmix = min_pmix, range_shape1 = range_shape1, range_shape2 = range_shape2,
+                    parametric = parametric, est_method = est_method, ci_method = ci_method, average = FALSE, control = control,
+                    hc = hc, save_to = save_to, samples = samples, fun = fun
+  )
+  
+  weight <- purrr::map_dbl(estimates, function(x) x$weight)
+  
+  hcp_ind_weight(hcp, weight = weight)
+}
--- R/hcp.R
+++ R/hcp.R
@@ -192,19 +192,6 @@ hcp_weighted <- function(hcp, level, samples, min_pboot) {
   hcp
 }
 
-hcp_ind <- function(hcp, weight) {
-  hcp <- mapply(
-    function(x, y) {
-      x$wt <- y
-      x
-    },
-    x = hcp, y = weight,
-    USE.NAMES = FALSE, SIMPLIFY = FALSE
-  )
-  hcp <- bind_rows(hcp)
-  hcp[c("dist", "value", "est", "se", "lcl", "ucl", "wt", "nboot", "pboot", "samples")]
-}
-
 replace_estimates <- function(hcp, est) {
   est <- dplyr::select(est, "value", est2 = "est")
   hcp <- dplyr::inner_join(hcp, est, by = "value")
@@ -262,26 +249,6 @@ replace_estimates <- function(hcp, est) {
   )
   hcp$dist <- "average"
   hcp[c("dist", "value", "est", "se", "lcl", "ucl", "wt", "nboot", "pboot", "samples")]
-}
-
-.ssd_hcp_ind <- function(x, value, ci, level, nboot, min_pboot, estimates,
-                         data, rescale,
-                         weighted, censoring, min_pmix, range_shape1,
-                         range_shape2, parametric,
-                         control, est_method, ci_method, hc, save_to, samples, fun) {
-  
-  hcp <- purrr::map(x, .ssd_hcp_tmbfit, weight = 1,
-                    value = value, ci = ci, level = level, nboot = nboot,
-                    min_pboot = min_pboot,
-                    data = data, rescale = rescale, weighted = weighted, censoring = censoring,
-                    min_pmix = min_pmix, range_shape1 = range_shape1, range_shape2 = range_shape2,
-                    parametric = parametric, est_method = est_method, ci_method = ci_method, average = FALSE, control = control,
-                    hc = hc, save_to = save_to, samples = samples, fun = fun
-  )
-  
-  weight <- purrr::map_dbl(estimates, function(x) x$weight)
-  
-  hcp_ind(hcp, weight = weight)
 }
 
 .ssd_hcp_fitdists_average <- function(
@@ -418,7 +385,7 @@ hcp2 <- function(
   }
   
   if (!average) {
-    hcp <- .ssd_hcp_ind(
+    hcp <- hcp_ind(
       x, value = value, ci = ci, level = level, nboot = nboot,
       min_pboot = min_pboot, estimates = estimates,
       data = data, rescale = rescale, weighted = weighted, censoring = censoring,
