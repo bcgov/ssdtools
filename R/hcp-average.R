f69078b46146306cede818067a93204a422cc667
Joe Thorley <joe@poissonconsulting.ca>
2025-06-07 19:45:49 +0800

    refactor

--- R/hcp-average.R
+++ R/hcp-average.R
@@ -1,16 +1,31 @@
+# Copyright 2015-2023 Province of British Columbia
+# Copyright 2021 Environment and Climate Change Canada
+# Copyright 2023-2025 Australian Government Department of Climate Change,
+# Energy, the Environment and Water
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 replace_estimates <- function(hcp, est) {
   dplyr::select(est, "value", est2 = "est") |>
     dplyr::inner_join(hcp, by = "value") |>
     dplyr::mutate(est = .data$est2, est2 = NULL)
 }
 
-hcp_noci <- function(value, nboot, est_method, ci_method, ...) {
+hcp_noci <- function(value, est_method, ci_method, ...) {
   tibble(
     value = value,
     est_method = est_method,
     ci_method = ci_method,
-    nboot = nboot,
-    pboot = 1
   )
 }
 
--- R/hcp-ind.R
+++ R/hcp-ind.R
@@ -1,3 +1,20 @@
+# Copyright 2015-2023 Province of British Columbia
+# Copyright 2021 Environment and Climate Change Canada
+# Copyright 2023-2025 Australian Government Department of Climate Change,
+# Energy, the Environment and Water
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#       https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 hcp_ind_weight <- function(hcp, weight) {
   mapply(function(x, y) {
     x$wt <- y
@@ -26,8 +43,6 @@ hcp_ind <- function(x, value, ci, level, nboot, min_pboot,
     hc = hc, save_to = save_to, samples = samples, fun = fun
   )
   weight <- glance(x, wt = TRUE)$wt
-  # 
-  # weight <- purrr::map_dbl(estimates, function(x) x$weight)
   
   hcp_ind_weight(hcp, weight = weight)
 }
